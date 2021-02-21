7240ebf2420a88b1dac9cc7a52d7361d903176a9
Pedro <pedrotzf@gmail.com>
2021-02-21 18:44:45 -0300

    rota de cadastro de usuários (ainda sem bcrypt e geração de token)

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -9,3 +9,22 @@ exports.getUsuarios = async (req, res) => {
         return res.status(500).send({ error: error });
     }
 };
+
+exports.criarUsuario = async (req, res) => {
+    try {
+        const query = `INSERT INTO usuarios
+                                   (nome_usuario, nome, email, cidade, uf, senha)
+                            VALUES (?, ?, ?, ?, ?, ?)`;
+        const resultado = await mysql.execute(query, [
+            req.body.nome_usuario,
+            req.body.nome,
+            req.body.email,
+            req.body.cidade,
+            req.body.uf,
+            req.body.senha
+        ]);
+        return res.status(200).send({ resultado: 'Usuário criado!' });
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+};
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usuariosController = require('../controllers/usuarios.controller');
 
 router.get('/', usuariosController.getUsuarios);
+router.post('/cadastro', usuariosController.criarUsuario);
 
 module.exports = router;
