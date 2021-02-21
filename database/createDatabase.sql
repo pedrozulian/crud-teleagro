9bea931c859c7bc31269d4bde952268da75b9bc5
Pedro <pedrotzf@gmail.com>
2021-02-21 18:36:09 -0300

    melhorando a tabela de usuário, add unique a nome_usuario e email

--- database/createDatabase.sql
+++ database/createDatabase.sql
@@ -4,11 +4,11 @@ USE teleagro;
 
 CREATE TABLE usuarios (
     id_usuario INT NOT NULL AUTO_INCREMENT,
-    nome_usuario VARCHAR(50) NOT NULL,
+    nome_usuario VARCHAR(50) NOT NULL UNIQUE,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     cidade VARCHAR(50),
-    uf VARCHAR(20),
+    uf VARCHAR(2),
     senha LONGTEXT NOT NULL,
     PRIMARY KEY (id_usuario)
 );
