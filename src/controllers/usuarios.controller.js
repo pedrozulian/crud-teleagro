1c46fd1a0f2a601478b2e4df21ac48172aca6234
Pedro <pedrotzf@gmail.com>
2021-02-21 18:57:06 -0300

    add encriptação de senha e tratando erro de e-mail já cadastrado

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,4 +1,6 @@
 const mysql = require('../config/db.config');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.getUsuarios = async (req, res) => {
     try {
@@ -12,19 +14,23 @@ exports.getUsuarios = async (req, res) => {
 
 exports.criarUsuario = async (req, res) => {
     try {
+        const hash = bcrypt.hashSync(req.body.senha, 10);
         const query = `INSERT INTO usuarios
                                    (nome_usuario, nome, email, cidade, uf, senha)
                             VALUES (?, ?, ?, ?, ?, ?)`;
         const resultado = await mysql.execute(query, [
             req.body.nome_usuario,
-            req.body.nome,
+            req.body.nome ? req.body.nome : req.body.nome_usuario,
             req.body.email,
             req.body.cidade,
             req.body.uf,
-            req.body.senha
+            hash
         ]);
         return res.status(200).send({ resultado: 'Usuário criado!' });
     } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') {
+            return res.status(406).send({ error: 'E-mail e/ou Nome Usuário já cadastrado' });
+        }
         return res.status(500).send({ error: error });
     }
 };
