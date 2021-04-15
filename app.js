3cf1d54c3e33c10e566d256ab59e0837449a7655
Pedro <pedrotzf@gmail.com>
2021-04-15 20:51:56 -0300

    feat(app.js): declare cors on app

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Root end-point - CRUD TeleAgro');
