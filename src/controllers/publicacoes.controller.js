f2122f46f3e4d3bb30145d000cfb7ae8b522879c
Pedro <pedrotzf@gmail.com>
2021-05-31 10:28:59 -0300

    feat(publicacoes): rota p adicionar nova publicação
    com texto e imagem

--- src/controllers/publicacoes.controller.js
+++ src/controllers/publicacoes.controller.js
@@ -13,3 +13,13 @@ exports.addComentario = async (req, res) => {
         return res.status(500).send({ error: error });
     }
 }
+
+exports.addPostagem = async (req, res) => {
+    try {
+        const query = `CALL pr_addPublicacao(?, ?, ?, ?);`;
+        resultado = await mysql.execute(query, [req.body.id_usuario, req.body.id_tipo_imagem, req.body.url_imagem, req.body.texto]);
+        return res.status(200).send({ mensagem: 'Nova publicação adicionada', status: 200 });
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+}
--- src/routes/publicacoes.routes.js
+++ src/routes/publicacoes.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const publicacoesController = require('../controllers/publicacoes.controller');
 const login = require('../middleware/login.moddleware');
 
+router.post('/', publicacoesController.addPostagem);
 router.post('/comentario', publicacoesController.addComentario);
 
 module.exports = router;
