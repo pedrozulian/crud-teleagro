ba98382326a9211713cbd94853ea72978ed11d27
Pedro <pedrotzf@gmail.com>
2021-06-08 19:01:02 -0300

    feat(publicacoes.route): rota que pega postagens do usuario

--- src/routes/publicacoes.routes.js
+++ src/routes/publicacoes.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const publicacoesController = require('../controllers/publicacoes.controller');
 const login = require('../middleware/login.moddleware');
 
+router.get('/:id_usuario', publicacoesController.getPostagensUsuario);
+
 router.post('/', publicacoesController.addPostagem);
 router.post('/comentario', publicacoesController.addComentario);
 
